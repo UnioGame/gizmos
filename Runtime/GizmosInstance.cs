56c388fcd0021cb6258b27fcd13f06153a77b3e6
Mefodei <mefodei.link@gmail.com>
2026-09-17 19:53:15 +0700

    Unity 6.6: remove obsolete sort overload

--- Runtime/GizmosInstance.cs
+++ Runtime/GizmosInstance.cs
@@ -95,7 +95,7 @@ namespace Popcron
             if (hotReloaded || !instance)
             {
                 bool markDirty = false;
-                GizmosInstance[] gizmosInstances = FindObjectsByType<GizmosInstance>(FindObjectsSortMode.None);
+                GizmosInstance[] gizmosInstances = FindObjectsByType<GizmosInstance>();
                 for (int i = 0; i < gizmosInstances.Length; i++)
                 {
                     instance = gizmosInstances[i];
